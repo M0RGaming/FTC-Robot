6c0625f35429b65400d1d86ecc660222edeeafea
M0R Gaming <masterofredstone3@gmail.com>
2017-11-20 21:17:39 -0700

    3
    added servos, added all 4 motors, added a frame to work auto mode inside

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/testbench.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/testbench.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -57,11 +28,13 @@ public class Test2 extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+    private DcMotor leftDriveForward = null;
+    private DcMotor rightDriveForward = null;
+    private DcMotor leftDriveBack = null;
+    private DcMotor rightDriveBack = null;
     private Servo rightClaw = null;
     private Servo leftClaw = null;
-    private Servo stinger = null;
+    private Servo shooter = null;
 
     @Override
     public void runOpMode() {
@@ -71,16 +44,29 @@ public class Test2 extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive_back");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive_back");
+        leftDriveBack  = hardwareMap.get(DcMotor.class, "left_drive_back");
+        rightDriveBack = hardwareMap.get(DcMotor.class, "right_drive_back");
+        leftDriveForward  = hardwareMap.get(DcMotor.class, "left_drive_forward");
+        rightDriveForward  = hardwareMap.get(DcMotor.class, "right_drive_forward");
         rightClaw = hardwareMap.get(Servo.class, "right_claw");
         leftClaw = hardwareMap.get(Servo.class, "left_claw");
         shooter = hardwareMap.get(Servo.class, "shooter");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftDriveForward.setDirection(DcMotor.Direction.FORWARD);
+        rightDriveForward.setDirection(DcMotor.Direction.REVERSE);
+        leftDriveBack.setDirection(DcMotor.Direction.FORWARD);
+        rightDriveBack.setDirection(DcMotor.Direction.REVERSE);
+
+        leftDriveForward.setPower(0);
+        rightDriveForward.setPower(0);
+        leftDriveBack.setPower(0);
+        rightDriveBack.setPower(0);
+
+        rightClaw.setPosition(0.5);
+        leftClaw.setPosition(0.5);
+        shooter.setPosition(0.5);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -97,6 +83,14 @@ public class Test2 extends LinearOpMode {
             double leftPos;
             double backPos;
 
+            boolean auto = true;
+
+            while (auto==true) {
+                if (gamepad1.a) {
+                    auto = false;
+                };
+            };
+
             // Choose to drive using either Tank Mode, or POV Mode
             // Comment out the method that's not used.  The default below is POV.
 
@@ -112,6 +106,8 @@ public class Test2 extends LinearOpMode {
             leftPower  = -gamepad1.left_stick_y ;
             rightPower = -gamepad1.right_stick_y ;
             
+
+// Cancels out any unnessicary movement.
             if(leftPower<0.1) {
                 leftPower = 0;
             };
@@ -121,13 +117,36 @@ public class Test2 extends LinearOpMode {
             };
 
             // Send calculated power to wheels
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
+            leftDriveForward.setPower(leftPower);
+            rightDriveForward.setPower(rightPower);
+            leftDriveBack.setPower(leftPower);
+            rightDriveBack.setPower(rightPower);
             
+
+// Moves the servos on the press on the buttons, hopefully it works
+
+            if(gamepad1.x) {
+
+                rightClaw.setPosition(0.25);
+                leftClaw.setPosition(0.75);
+
+            } else if (gamepad1.b) {
+
+                rightClaw.setPosition(0.75);
+                leftClaw.setPosition(0.25);
+
+            } else if (gamepad1.start) {
+
+                shooter.setPosition(0.75);
+
+            };
             
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Left Claw Pos", leftClaw.getPosition());
+            telemetry.addData("Right Claw Pos", rightClaw.getPosition());
+            telemetry.addData("Shooter Pos", shooter.getPosition());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.update();
         }
